36c65a45997950affca06575c90c945caa38192f
LuanSilva9 <luanpdev@gmail.com>
2024-09-22 20:51:10 -0400

    Criando Model Investiment

---
+++ app/Models/Investiment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Investiment extends Model
+{
+    use HasFactory;
+
+    protected $table = "investiment";
+    public $timestamps = false;
+
+    protected $fillable = [
+        "name_investiment",
+        "current_investiment",
+        "session_id",
+        "default_color",
+        "default_icon",
+    ];
+
+    public function session() {
+        return $this->belongsTo(Session::class, 'session_id');
+    }
+}
