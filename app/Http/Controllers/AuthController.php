b3fd275cfe074d295dd09ee7759c030b8d889ffb
LuanSilva9 <luanpdev@gmail.com>
2024-10-07 13:40:35 -0400

    Comecando a construir o processo de autenticacao

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return User::all();
+    }
+
+    public function auth(Request $request) {
+        $request->validate([
+            "email" => "required",
+            "password" => "required"
+        ]);
+
+        $user = User::where("email", $request->email)->first();
+        
+        if($user && Hash::check($request->password, $user->password)) {
+            return $user;
+        }
+
+        return "nao existe";
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,29 +4,32 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\InvestimentController;
+use App\Http\Controllers\AuthController;
 
 
 Route::get('/', function () {
     return "Hello, World!";
 });
 
-Route::prefix('session')->name('session.')->group(function () {
-    Route::get('/', function() { return redirect('session/view'); });
-
-    Route::get('/view', [SessionController::class, 'index'])->name('view');
-    Route::post('/new', [SessionController::class, 'create'])->name('create');
-    Route::put('/edit/name/{id_session}', [SessionController::class, 'edit_name'])->name('edit_name');
-    Route::put('/edit/id/{id_session}', [SessionController::class, 'edit_id'])->name('edit_id');
-    Route::delete('/destroy/{id_session}', [SessionController::class, 'destroy'])->name('destroy');
-    Route::get('/view/{id}', [SessionController::class, 'show'])->name('show');
-});
-
-Route::prefix('investiment')->name('investiment.')->group(function() {
-    Route::get('/', function() { return redirect('investiment/view'); });
-
-    Route::get('/view', [InvestimentController::class, 'index'])->name('view');
-    Route::post('/new', [InvestimentController::class, 'create'])->name('create');
-    Route::get('/view/{id}', [InvestimentController::class, 'show'])->name('show');
-    Route::put('/edit/{id}', [InvestimentController::class, 'edit'])->name('edit');
-    Route::delete('/destroy/{id}', [InvestimentController::class, 'destroy'])->name('destroy');
+Route::prefix('api')->group(function() {
+    Route::prefix('session')->name('session.')->group(function () {
+        Route::get('/view', [SessionController::class, 'index'])->name('view');
+        Route::post('/new', [SessionController::class, 'create'])->name('create');
+        Route::put('/edit/name/{id_session}', [SessionController::class, 'edit_name'])->name('edit_name');
+        Route::put('/edit/id/{id_session}', [SessionController::class, 'edit_id'])->name('edit_id');
+        Route::delete('/destroy/{id_session}', [SessionController::class, 'destroy'])->name('destroy');
+        Route::get('/view/{id}', [SessionController::class, 'show'])->name('show');
+    });
+    
+    Route::prefix('investiment')->name('investiment.')->group(function() {
+        Route::get('/view', [InvestimentController::class, 'index'])->name('view');
+        Route::post('/new', [InvestimentController::class, 'create'])->name('create');
+        Route::get('/view/{id}', [InvestimentController::class, 'show'])->name('show');
+        Route::put('/edit/{id}', [InvestimentController::class, 'edit'])->name('edit');
+        Route::delete('/destroy/{id}', [InvestimentController::class, 'destroy'])->name('destroy');
+    });
+    
+    Route::prefix('auth')->name('auth.')->group(function() {
+        Route::post('/login', [AuthController::class, 'auth'])->name('auth');
+    });
 });
